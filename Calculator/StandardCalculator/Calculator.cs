b4466d7f2f7055d0e8809aaa98a669a4c1a42466
fiatbux <43115426+fiatbux@users.noreply.github.com>
2022-01-16 16:49:12 +0100

    Add files via upload
    update

---
+++ Calculator/StandardCalculator/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardCalculator
+{
+    public class Calculator : ICalculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ Calculator/StandardCalculator/ICalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace StandardCalculator
+{
+    public interface ICalculator
+    {
+        double Add(double x, double y);
+
+        double Subtract(double x, double y);
+
+        double Multiply(double x, double y);
+    }
+}
