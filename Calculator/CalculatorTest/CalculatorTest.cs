80ff35c0bbdfc5b386e05f547e95227ae96df0ab
fiatbux <43115426+fiatbux@users.noreply.github.com>
2022-01-16 16:47:00 +0100

    Add files via upload
    Calculator Unit testing

---
+++ Calculator/CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,40 @@
+﻿
+
+using NUnit.Framework;
+using StandardCalculator;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        Calculator calculator;
+
+        [SetUp]
+        public void Init()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void AddTest()
+        {
+            const double expected = 3;
+            var actual = calculator.Add(1, 2);
+
+            Assert.AreEqual(expected, actual, "Sum of doubles is not equal");
+        }
+
+        [TestCase(3,3, 9,"Standard multiplication of 3 by 3")]
+        [TestCase(2.5, 5.5, 13.75)]
+        [TestCase(-2.5, -5.5, 13.75)]
+        [TestCase(2.5, -5.5, -13.75)]
+        public void MultiplyTest(double x, double y, double expected)
+        {
+
+            var actual = calculator.Multiply(x, y);
+
+            Assert.AreEqual(expected, actual, "Sum of doubles is not equal");
+        }
+    }
+}
